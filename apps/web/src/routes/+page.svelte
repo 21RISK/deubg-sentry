ef52ef60922a64a5632c112d3872e04e51b60f8b
Lukas Stracke <lukas.stracke@sentry.io>
2023-07-17 10:01:01 +0200

    initial commit

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { MyCounterButton } from "ui";
+</script>
+
+<h1>Web</h1>
+<MyCounterButton />
+
+<button
+  on:click={() => {
+    throw new Error("Is it sourcemapped, though?");
+  }}
+>
+  Throw test error
+</button>
+
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
---
+++ packages/ui/components/MyCounterButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<button on:click={handleClick} style="background-color: red;">
+  clicks: {count}
+</button>
